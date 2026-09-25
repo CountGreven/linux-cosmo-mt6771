5f02d71da03b4def44e792ebc46efe2820c3cfbf
Fredrik Lindlöf <fredrik.lindlof@gmail.com>
2026-09-25 17:30:49 +0200

    fix(mt6771-consys): gl_p2p_cfg80211.c: get_tx_power radio_idx/link_id
    API delta (cfg80211, 6.x): get_tx_power gained radio_idx and link_id; unused, one radio and one link.

--- drivers/net/wireless/mediatek/mt6771-consys/wlan/core/gen3/os/linux/gl_p2p_cfg80211.c
+++ drivers/net/wireless/mediatek/mt6771-consys/wlan/core/gen3/os/linux/gl_p2p_cfg80211.c
@@ -703,7 +703,7 @@ int mtk_p2p_cfg80211_set_txpower(struct wiphy *wiphy,
 
 int mtk_p2p_cfg80211_get_txpower(struct wiphy *wiphy,
 				 struct wireless_dev *wdev,
-				 int *dbm)
+				 int radio_idx, unsigned int link_id, int *dbm)
 {
 	P_GLUE_INFO_T prGlueInfo = NULL;
 

55031fdc61a001a427d94665b8635fcc8776ee09
Fredrik Lindlöf <fredrik.lindlof@gmail.com>
2026-09-25 17:06:42 +0200

    fix(mt6771-consys): osal_typedef.h: aee.h -> shim
    API delta: the vendor header mt-plat/aee.h is not in mainline; its services now come from mtk_plat_shim.h (see projects/cosmo/17-consys-port.org). No logic changed.

--- drivers/net/wireless/mediatek/mt6771-consys/common/common_main/linux/include/osal_typedef.h
+++ drivers/net/wireless/mediatek/mt6771-consys/common/common_main/linux/include/osal_typedef.h
@@ -42,7 +42,7 @@
 #include <linux/jiffies.h>
 #include <linux/slab.h>
 #if defined(WMT_PLAT_ALPS) && WMT_PLAT_ALPS
-#include <aee.h>
+#include <mtk_plat_shim.h>
 #endif
 #include <linux/kfifo.h>
 #include <linux/device.h>
